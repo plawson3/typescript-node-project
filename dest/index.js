1886c375730b17b19eab51362fb4d2cb0e9bb734
plawson3 <plawson346@gmail.com>
2023-01-14 17:27:56 +0500

    Initial Commit

--- dest/index.js
+++ dest/index.js
@@ -1,123 +1,27 @@
 "use strict";
-const data = [
-    {
-        question: `1. Javascript is an _______ language?`,
-        a: "Object-Oriented",
-        b: "Object-Based",
-        c: 'Procedural',
-        d: 'None of the above',
-        answer: 'a'
-    },
-    {
-        question: "2. Which of the following keywords is used to define a variable in Javascript?",
-        a: "var",
-        b: "let",
-        c: 'Both a and b',
-        d: 'None of the above',
-        answer: 'c'
-    },
-    {
-        question: "3. Which of the following methods is used to access HTML elements using Javascript?",
-        a: "getElementbyId()",
-        b: "getElementsByClassName()",
-        c: 'Both a and b',
-        d: 'None of the above',
-        answer: 'c'
-    },
-    {
-        question: "4. Upon encountering empty statements, what does the Javascript Interpreter do?",
-        a: "Throws an error",
-        b: "Ignores the statements",
-        c: 'Gives a warning',
-        d: 'None of the above',
-        answer: 'b'
-    },
-    {
-        question: "5. Which of the following methods can be used to display data in some form using Javascript?",
-        a: "document.write()",
-        b: "console.log()",
-        c: 'window.alert()',
-        d: 'All of the above',
-        answer: 'd'
-    },
-];
-const startContainer = document.getElementById('startPortion');
-const quizContainer = document.getElementById('questionPortion');
-const radioButtons = document.querySelectorAll('input[type="radio"]');
-const submitBtn = document.getElementById('showScore');
-const mainDiv = document.getElementById('main');
-let countAnswers = 0;
-let qno = 0;
-function ShowResult() {
-    mainDiv.style.marginTop = '6%';
-    toggleVisibility();
+let date_ob = new Date();
+// current date
+let date = ("0" + date_ob.getDate()).slice(-2);
+// current month
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+// current year
+let year = date_ob.getFullYear();
+let dateDiv = document.getElementById('_date');
+dateDiv.innerHTML = `${date}-${month}-${year}`;
+function getCurrentTime() {
+    const clockElement = document.getElementById('_time');
+    clockElement.innerHTML = new Date().toLocaleTimeString();
 }
-function toggleVisibility() {
-    var div = document.getElementById("showResult");
-    if (div.style.display === "none") {
-        div.style.display = "block";
-    }
-    else {
-        submitBtn.classList.remove('disabled');
-        div.style.display = "none";
-        mainDiv.style.marginTop = '10%';
-        countAnswers = 0;
-        qno = 0;
-        DisplayQuestion(qno);
-    }
-}
-function DisplayQuestion(n) {
-    let question = document.getElementById('question');
-    let ans1 = document.getElementById('lbl_ans1');
-    let ans2 = document.getElementById('lbl_ans2');
-    let ans3 = document.getElementById('lbl_ans3');
-    let ans4 = document.getElementById('lbl_ans4');
-    if (n < data.length) {
-        let QuestionNo = data[n];
-        question.innerHTML = QuestionNo.question;
-        ans1.innerHTML = QuestionNo.a;
-        ans2.innerHTML = QuestionNo.b;
-        ans3.innerHTML = QuestionNo.c;
-        ans4.innerHTML = QuestionNo.d;
-    }
-}
-// function deSelectAll() {
-//     radioButtons.forEach((e) => {
-//         for (let i = 0; i < radioButtons.length; i++) {
-//             const radioButton = radioButtons[i] as HTMLInputElement;
-//             radioButton.checked = false;
-//         }
-//     })
-// }
-const getAnswer = () => {
-    let answer = '';
-    radioButtons.forEach((e) => {
-        for (let i = 0; i < radioButtons.length; i++) {
-            const radioButton = radioButtons[i];
-            if (radioButton.checked) {
-                answer = radioButton.id;
-            }
-        }
-    });
-    return answer;
+setInterval(getCurrentTime, 1000);
+const _countDown = document.getElementById('countdown');
+const newDate = new Date();
+let hour = newDate.getHours();
+let minutes = newDate.getMinutes();
+let time = minutes * 60;
+const updateCountDown = () => {
+    const _minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    _countDown.innerHTML = `${hour}:${_minutes}:${seconds}`;
+    time--;
 };
-submitBtn.addEventListener('click', () => {
-    if (qno < data.length) {
-        let checkedAnswer = getAnswer();
-        console.log(checkedAnswer, data[qno].answer, qno, countAnswers);
-        if (checkedAnswer === data[qno].answer) {
-            countAnswers = countAnswers + 1;
-        }
-        qno++;
-        DisplayQuestion(qno);
-    }
-    else {
-        const score = document.getElementById('score');
-        score.innerHTML = String(`${countAnswers}/5`);
-        ShowResult();
-        submitBtn.classList.add('disabled');
-    }
-});
-document.addEventListener("DOMContentLoaded", () => {
-    DisplayQuestion(0);
-});
+setInterval(updateCountDown, 1000);
